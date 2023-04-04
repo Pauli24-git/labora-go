ec3c00d9f94a8b0c97555c37158d09a57f3dc812
Pauli <pau2494@gmail.com>
2023-04-04 00:30:12 -0300

    commit sistema solar

---
+++ seccion-1/variables/sistema-solar.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	planet := "Mercurio"
+	Moons := 0
+	fmt.Println(planet, Moons)
+
+	planet = "Venus"
+	Moons = 0
+	fmt.Println(planet, Moons)
+
+	planet = "Tierra"
+	Moons = 1
+	fmt.Println(planet, Moons)
+
+	planet = "Marte"
+	Moons = 2
+	fmt.Println(planet, Moons)
+
+	planet = "Jupiter"
+	Moons = 63
+	fmt.Println(planet, Moons)
+
+	planet = "Saturno"
+	Moons = 62
+	fmt.Println(planet, Moons)
+
+	planet = "Urano"
+	Moons = 27
+	fmt.Println(planet, Moons)
+
+	planet = "Neptuno"
+	Moons = 13
+	fmt.Println(planet, Moons)
+}
